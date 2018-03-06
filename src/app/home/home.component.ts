6f8947ced6245efb0cdf25d4945c4fb622646831
Marshall Munoz <Marshall.Munoz@fbitn.com>
2018-03-06 12:07:23 -0600

    tasks can now be removed with an animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,13 @@ import {trigger, style, transition, animate, keyframes, query, stagger} from '@a
             style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1.0}),
           ]))]), { optional: true})
+          ,
+          query(':leave', stagger('300ms',[
+            animate('.6s ease-out', keyframes([
+              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+              style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
+              style({opacity: 0, transform: 'translateY(-75%)', offset: 1.0 }),
+            ]))]), { optional: true})
       ])
     ])
 
@@ -38,6 +45,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = ' ';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 
 }
